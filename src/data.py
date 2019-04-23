915bbf309f1ece9018a4ca75b4fc9d9e06b70922
Reno Thiart <rthiart@seas.upenn.edu>
2019-04-23 12:11:22 -0400

    added command line option for data

--- src/data.py
+++ src/data.py
@@ -1,4 +1,11 @@
+import sys
 import pandas as pd
+
+# allow for command line specification of rows
+if (len(sys.argv) > 1):
+    num_rows = int(sys.argv[1])
+else:
+    num_rows = 200
 
 # read in marvel images
 m_data = pd.read_csv('../data/marvel-wikia-data-images.csv', delimiter=',')
@@ -9,7 +16,7 @@ d_data = pd.read_csv('../data/dc-wikia-data-images.csv', delimiter=',')
 dc_data = d_data['imgurl'].dropna().reset_index(drop=True)
 
 # merge the data
-data_series = pd.concat([marvel_data.head(100), dc_data.head(100)])
+data_series = pd.concat([marvel_data.head(num_rows // 2), dc_data.head(num_rows // 2)])
 data = data_series.to_frame().reset_index(drop=True)
 
 # read prompt word csv
@@ -23,8 +30,8 @@ p_words = long_words.sample(frac=1)
 prompt_words = p_words.dropna().reset_index(drop=True)
 
 # join and write
-joined = data.join(prompt_words.head(200))
+joined = data.join(prompt_words.head(num_rows))
 final = joined.rename(index=str, columns={'imgurl':'image_url', 'prompt':'prompt'})
 
 # write to csv
-final.to_csv('../data/mturk_input_200.csv', index=False)
+final.to_csv('../data/mturk_input_{}.csv'.format(num_rows), index=False)
